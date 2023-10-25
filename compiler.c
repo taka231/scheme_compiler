7a467ef6733fb82d7956d8c246cf0aa7003c89ed
taka231 <taka23131@gmail.com>
2023-10-26 01:03:18 +0900

    parserが一通り完成した

--- compiler.c
+++ compiler.c
@@ -9,10 +9,8 @@ typedef enum {
   TK_LPAREN,
   TK_RPAREN,
   TK_DOT,
+  TK_SINGLE_QUOTE,
   TK_SYMBOL,
-  TK_DEFINE,
-  TK_LET,
-  TK_LAMBDA,
   TK_NUM,
   TK_EOF,
 } TokenKind;
@@ -42,17 +40,11 @@ void print_token(Token *token) {
     case TK_DOT:
       printf("DOT\n");
       break;
+    case TK_SINGLE_QUOTE:
+      printf("SINGLE_QUOTE\n");
+      break;
     case TK_SYMBOL:
       printf("SYMBOL %.*s\n", token->length, token->str);
-      break;
-    case TK_DEFINE:
-      printf("DEFINE\n");
-      break;
-    case TK_LET:
-      printf("LET\n");
-      break;
-    case TK_LAMBDA:
-      printf("LAMBDA\n");
       break;
     case TK_NUM:
       printf("NUM %d\n", token->val);
@@ -83,12 +75,34 @@ bool consume_lparen() {
   return true;
 }
 
+void expect_lparen() {
+  if (token->kind != TK_LPAREN) {
+    error("expected '('");
+  }
+  token = token->next;
+}
+
 bool consume_rparen() {
   if (token->kind != TK_RPAREN) {
     return false;
   }
   token = token->next;
   return true;
+}
+
+bool consume_single_quote() {
+  if (token->kind != TK_SINGLE_QUOTE) {
+    return false;
+  }
+  token = token->next;
+  return true;
+}
+
+void expect_rparen() {
+  if (token->kind != TK_RPAREN) {
+    error("expected ')'");
+  }
+  token = token->next;
 }
 
 int expect_number() {
@@ -140,6 +154,12 @@ Token *tokenize(char *p) {
       continue;
     }
 
+    if (*p == '\'') {
+      cur = new_token(TK_SINGLE_QUOTE, cur, p);
+      p++;
+      continue;
+    }
+
     if (*p == '-' && isdigit(*(p + 1))) {
       cur = new_token(TK_NUM, cur, p);
       p++;
@@ -160,24 +180,6 @@ Token *tokenize(char *p) {
       } else {
         cur->val = num;
       }
-      continue;
-    }
-
-    if (strncmp(p, "define", 6) == 0 && !isalnum(p[6])) {
-      cur = new_token(TK_DEFINE, cur, p);
-      p += 6;
-      continue;
-    }
-
-    if (strncmp(p, "let", 3) == 0 && !isalnum(p[3])) {
-      cur = new_token(TK_LET, cur, p);
-      p += 3;
-      continue;
-    }
-
-    if (strncmp(p, "lambda", 6) == 0 && !isalnum(p[6])) {
-      cur = new_token(TK_LAMBDA, cur, p);
-      p += 6;
       continue;
     }
 
@@ -251,8 +253,11 @@ void print_node(Node *node) {
     printf("ND_LET");
     printf("(");
     for (int i = 0; i < node->symbols_length; i++) {
+      printf("(");
       print_node(node->symbols[i]);
       printf(", ");
+      print_node(node->let_value[i]);
+      printf("),");
     }
     printf("; BODY:");
     print_node(node->body);
@@ -307,7 +312,7 @@ Node *new_node_define(Node *define_symbol, Node *body) {
 }
 
 Node *new_node_let(Node **symbols, int symbols_length, Node **let_value,
-                   Node *body, int body_length) {
+                   Node *body) {
   Node *node = calloc(1, sizeof(Node));
   node->kind = ND_LET;
   node->symbols = symbols;
@@ -335,30 +340,184 @@ Node *new_node_list(Node **item, int item_length) {
   return node;
 }
 
+Node *expr();
+
+Node *symbol() {
+  if (token->kind == TK_SYMBOL) {
+    Node *symbol = new_node_symbol(token->str, token->length);
+    token = token->next;
+    return symbol;
+  } else {
+    error("expected symbol");
+  }
+}
+
+Node *num() {
+  if (token->kind == TK_NUM) {
+    Node *num = new_node_num(token->val);
+    token = token->next;
+    return num;
+  } else {
+    error("expected number");
+  }
+}
+
+Node *inner_list() {
+  int max_item_length = 10;
+  Node **item = calloc(max_item_length, sizeof(Node *));
+  int item_length = 0;
+  while (!consume_rparen()) {
+    Node *node = expr();
+    item[item_length] = node;
+    item_length++;
+    if (item_length > max_item_length) {
+      Node **new_item = realloc(item, max_item_length * 2);
+      if (new_item == NULL) {
+        error("failed to realloc");
+      }
+      item = new_item;
+      max_item_length *= 2;
+    }
+    if (at_eof()) {
+      error("expected ')'");
+    }
+  }
+  return new_node_list(item, item_length);
+}
+
+Node *list() {
+  if (consume_lparen()) {
+    return inner_list();
+  } else {
+    error("expected list");
+  }
+}
+
+Node *const_value();
+
+Node *quote() {
+  if (consume_single_quote()) {
+    Node *quote = new_node_symbol("quote", 5);
+    Node *node = const_value();
+    Node **item = calloc(2, sizeof(Node *));
+    item[0] = quote;
+    item[1] = node;
+    return new_node_list(item, 2);
+  } else {
+    error("expected quote");
+  }
+}
+
+Node *const_value() {
+  if (token->kind == TK_NUM) {
+    return num();
+  }
+  if (token->kind == TK_SYMBOL) {
+    return symbol();
+  }
+  if (token->kind == TK_LPAREN) {
+    return list();
+  }
+  if (token->kind == TK_SINGLE_QUOTE) {
+    return quote();
+  }
+  error("expected const value");
+}
+
 Node *expr() {
   if (consume_lparen()) {
-    if (token->kind == TK_DEFINE) {
+    if (token->kind == TK_SYMBOL &&
+        strncmp(token->str, "define", token->length) == 0) {
       token = token->next;
       Node *define_symbol = expr();
       if (define_symbol->kind != ND_SYMBOL) {
         error("expected symbol");
       }
       Node *body = expr();
-      if (!consume_rparen()) {
-        error("expected ')'");
-      }
+      expect_rparen();
       return new_node_define(define_symbol, body);
     }
+    if (token->kind == TK_SYMBOL &&
+        strncmp(token->str, "lambda", token->length) == 0) {
+      token = token->next;
+      int max_symbols_length = 10;
+      Node **symbols = calloc(max_symbols_length, sizeof(Node *));
+      int symbols_length = 0;
+      expect_lparen();
+      while (!consume_rparen()) {
+        Node *symbol = expr();
+        symbols[symbols_length] = symbol;
+        symbols_length++;
+        if (symbols_length > max_symbols_length) {
+          Node **new_symbols = realloc(symbols, max_symbols_length * 2);
+          if (new_symbols == NULL) {
+            error("failed to realloc");
+          }
+          symbols = new_symbols;
+          max_symbols_length *= 2;
+        }
+        if (at_eof()) {
+          error("expected ')'");
+        }
+      }
+      Node *body = expr();
+      expect_rparen();
+      return new_node_lambda(symbols, symbols_length, body, 1);
+    }
+    if (token->kind == TK_SYMBOL &&
+        strncmp(token->str, "let", token->length) == 0) {
+      token = token->next;
+      int max_symbols_length = 10;
+      Node **symbols = calloc(max_symbols_length, sizeof(Node *));
+      Node **let_value = calloc(max_symbols_length, sizeof(Node *));
+      int symbols_length = 0;
+      expect_lparen();
+      while (!consume_rparen()) {
+        expect_lparen();
+        Node *symbol = expr();
+        if (symbol->kind != ND_SYMBOL) {
+          error("expected symbol");
+        }
+        Node *value = expr();
+        symbols[symbols_length] = symbol;
+        let_value[symbols_length] = value;
+        symbols_length++;
+        if (symbols_length > max_symbols_length) {
+          Node **new_symbols = realloc(symbols, max_symbols_length * 2);
+          Node **new_let_value = realloc(let_value, max_symbols_length * 2);
+          if (new_symbols == NULL || new_let_value == NULL) {
+            error("failed to realloc");
+          }
+          symbols = new_symbols;
+          let_value = new_let_value;
+          max_symbols_length *= 2;
+        }
+        if (at_eof()) {
+          error("expected ')'");
+        }
+        expect_rparen();
+      }
+      Node *body = expr();
+      expect_rparen();
+      return new_node_let(symbols, symbols_length, let_value, body);
+    }
+    return inner_list();
+  }
+  if (token->kind == TK_LPAREN) {
+    Node *list_ = list();
+    return list_;
   }
   if (token->kind == TK_SYMBOL) {
-    Node *symbol = new_node_symbol(token->str, token->length);
-    token = token->next;
-    return symbol;
+    Node *symbol_ = symbol();
+    return symbol_;
   }
   if (token->kind == TK_NUM) {
-    Node *num = new_node_num(token->val);
-    token = token->next;
-    return num;
+    Node *num_ = num();
+    return num_;
+  }
+  if (token->kind == TK_SINGLE_QUOTE) {
+    Node *quote_ = quote();
+    return quote_;
   }
   error("unimplemented");
 }
@@ -379,3 +538,22 @@ int main(int argc, char **argv) {
 
   return 0;
 }
+
+typedef enum IRKind {
+  IR_ID,
+  IR_IDC,
+  IR_IDG,
+  IR_IDF,
+  IR_ARGS,
+  IR_APP,
+  IR_RTN,
+  IR_SEL,
+  IR_JOIN,
+  IR_POP,
+  IR_DEF,
+  IR_STOP,
+} IRKind;
+
+typedef struct IR {
+  IRKind kind;
+} IR;
